c8adeab3139a941b08392d16bcaadcf24bab2c96
heydoyouknowme0 <akrb2204@gmail.com>
2023-11-13 22:23:47 +0530

    Added tailwind MUI Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,38 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
+import ChatIcon from '@mui/icons-material/Chat';
+import HomeIcon from '@mui/icons-material/Home';
+import BudgetIcon from '@mui/icons-material/PointOfSale';
+import TasksIcon from '@mui/icons-material/Assignment';
+import { Paper } from '@mui/material';
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const App = () => {
+  const [value, setValue] = useState(0);
 
   return (
-    <>
-    </>
-  )
+    <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={2}>
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+
+      >
+        <BottomNavigationAction label="Shopping List" value={-2} icon={<ShoppingBagIcon />} />
+        <BottomNavigationAction label="Chat" value={-1} icon={<ChatIcon />} />
+        <BottomNavigationAction label="Home" value={0} icon={<HomeIcon />} />
+        <BottomNavigationAction label="Budget" value={1} icon={<BudgetIcon />} />
+        <BottomNavigationAction label="Tasks" value={2} icon={<TasksIcon />} />
+
+      </BottomNavigation>
+      </Paper>
+
+  );
 }
+
 
 export default App
