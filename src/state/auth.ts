00a73ef4bd54ab6ae396514d1bedfdfca2891355
heydoyouknowme0 <akrb2204@gmail.com>
2023-11-26 17:42:10 +0530

    short fix 2

--- src/state/auth.ts
+++ src/state/auth.ts
@@ -1,7 +1,7 @@
 import { atom } from 'recoil';
 
 // Retrieve user from local storage and parse JSON, default to an empty object if null
-const initialUser = JSON.parse(localStorage.getItem('user') || 'null') || {};
+const initialUser = JSON.parse(localStorage.getItem('user') || 'null');
 
 const authAtom = atom({
     key: 'auth',
